1a0e0a22d69896fdcde186c8758d7faa8443f671
SulemanNiazzi <163248606+sulemanAzeemKhan7668@users.noreply.github.com>
2026-02-17 13:41:47 +0500

    update singleton

--- lib/singleton_class.dart
+++ lib/singleton_class.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 import 'dart:developer';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
